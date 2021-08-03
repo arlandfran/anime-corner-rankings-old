b0f0e68155c6780a113086e1c064664b5c06f1cb
Arland Torres <arland.torres@outlook.com>
2021-08-03 19:46:31 +0100

    Add basic styling for page and card component (#9)

--- src/Item.svelte
+++ src/Item.svelte
@@ -4,8 +4,28 @@
   export let votes;
 </script>
 
-<div>Rank: {rank} {title} {votes}</div>
+<div class="card">
+  <div class="card--rank">
+    {rank}
+  </div>
+  <div class="card--title">
+    {title}
+  </div>
+  <div class="card--votes">
+    {votes}%
+  </div>
+</div>
 
 <style>
-  /* your styles go here */
+  .card {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem;
+    height: 4rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    background-color: var(--primary-color);
+    border-radius: 4px;
+  }
 </style>
