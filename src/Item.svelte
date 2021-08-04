1958d058b7ee55da075d76704eae623c3efb1801
Arland Torres <arland.torres@outlook.com>
2021-08-04 21:26:38 +0100

    Dynamically create div on click for item component

--- src/Item.svelte
+++ src/Item.svelte
@@ -2,9 +2,26 @@
   export let rank;
   export let title;
   export let votes;
+
+  let parentNode;
+
+  // https://stackoverflow.com/questions/4793604/how-to-insert-an-element-after-another-element-in-javascript-without-using-a-lib
+  function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
+
+  function addDiv() {
+    // const card = document.getElementsByClassName("card")[0];
+    console.log(document.getElementsByClassName("card")[0]);
+
+    const div = document.createElement("div");
+    const text = document.createTextNode("Anime details...");
+    div.appendChild(text);
+    insertAfter(parentNode, div);
+  }
 </script>
 
-<div class="card">
+<div class="card" on:click={addDiv} bind:this={parentNode}>
   <div class="card--rank">
     {rank}
   </div>
