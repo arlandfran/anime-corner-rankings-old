19e87247b8bb21a7bec97b589224452689cee9d7
arland <arland.torres@outlook.com>
2021-08-10 01:48:13 +0100

    Extract caching logic to cache.js (#25)

---
+++ src/cache.js
@@ -0,0 +1,30 @@
+/**
+ * Checks if cachedData exists and if true returns cached data and expiry status.
+ * @param {string} key localStorage Key that you want to retrieve the value of.
+ */
+export function checkCache(key) {
+  const cacheLife = 86400; // Set cache lifetime to 24 hours in seconds
+  let expired;
+  let cachedData = localStorage.getItem(`${key}`); // Get cached data from local storage
+
+  // If cached data exists then parse the data and check if data is expired
+  if (cachedData) {
+    cachedData = JSON.parse(cachedData);
+    expired = parseInt(Date.now() / 1000) - cachedData.cachetime > cacheLife;
+    console.log(`${key} expired:`, expired);
+  }
+  return {
+    cachedData,
+    expired,
+  };
+}
+
+/**
+ *  Takes a key and an object array that will be converted to a JSON string and saves that key: value pair in localStorage.
+ * @param {string} key localStorage key that you want to create.
+ * @param {array} data Array that will be converted to JSON and used as the value for the key you are creating.
+ */
+export function cacheData(key, data) {
+  const cacheData = { data: data, cachetime: parseInt(Date.now() / 1000) };
+  localStorage.setItem(`${key}`, JSON.stringify(cacheData));
+}
