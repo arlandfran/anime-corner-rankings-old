fb40cc59012c9fe478234bf5583dd73384195ae8
arland <arland.torres@outlook.com>
2021-08-10 01:49:15 +0100

    Refactor caching for API response (#25)

--- src/Item.svelte
+++ src/Item.svelte
@@ -1,5 +1,6 @@
 <script>
   import axios from "axios";
+  import { checkCache, cacheData } from "./cache";
 
   export let rank;
   export let title;
@@ -12,23 +13,12 @@
 
   const fetchDetails = async () => {
     let data;
-
-    // Set cache lifetime in seconds
-    let cacheLife = 86400; // 24 hours
-    // Get cached data from local storage
-    let cachedData = localStorage.getItem(`${title} Details`);
-
-    // If cached data exists then parse the data and check if data is expired
-    if (cachedData) {
-      cachedData = JSON.parse(cachedData);
-      var expired =
-        parseInt(Date.now() / 1000) - cachedData.cachetime > cacheLife;
-      console.log("Cached Data expired:", expired);
-    }
+    let key = `${title} Details`;
+    let cache = checkCache(key);
 
     // If cached data exists and is not expired then return data
-    if (cachedData && !expired) {
-      return cachedData.data;
+    if (cache.cachedData && !cache.expired) {
+      return cache.cachedData.data;
     } else {
       // Otherwise fetch data from Anilist
       const query = `
@@ -101,8 +91,7 @@
           data = await fetchAnimeKitsu();
         });
       // Save data in local storage
-      let cacheData = { data: data, cachetime: parseInt(Date.now() / 1000) };
-      localStorage.setItem(`${title} Details`, JSON.stringify(cacheData));
+      cacheData(key, data);
 
       console.log(`${title} details fetched`, data);
       return data;
