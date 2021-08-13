ea7ccc09f9c2fe838745d26c9edc22ec371a6554
Arland Torres <arland.torres@outlook.com>
2021-08-13 16:34:01 +0100

    Add rank progression and vote difference

--- src/components/ItemDetails.svelte
+++ src/components/ItemDetails.svelte
@@ -1,12 +1,110 @@
 <script>
+  import { db } from "../firebase";
+  import { year, season, week } from "../stores";
+  export let rank;
+  export let title;
   export let votes;
+
+  function decrementWeek(week) {
+    let n = parseInt(week.split("-")[1]);
+    n = n - 1;
+
+    if (n > 10) {
+      return "Week-" + n.toString();
+    } else {
+      return "Week-0" + n.toString();
+    }
+  }
+
+  function calculateRankProgression(currentRank, previousRank) {
+    if (currentRank < previousRank) {
+      return `⮅ ${previousRank - currentRank}`;
+    } else {
+      return `⮇ ${(previousRank - currentRank) * -1}`;
+    }
+  }
+
+  function calculateVoteDifference(currentVotes, previousVotes) {
+    let difference = currentVotes - previousVotes;
+    if (difference > 0) {
+      return `+${difference.toFixed(2)} GAIN`;
+    } else {
+      return `${difference.toFixed(2)} DROP`;
+    }
+  }
+
+  const fetchDetails = async () => {
+    let details;
+
+    const previousRankQuery = db
+      .collection($year)
+      .doc($season)
+      .collection(decrementWeek($week))
+      .where("title", "==", title);
+
+    let previousRanks = await previousRankQuery
+      .get()
+      .then((snapshots) => snapshots.docs.map((doc) => doc.data().rank));
+
+    const previousVoteQuery = db
+      .collection($year)
+      .doc($season)
+      .collection(decrementWeek($week))
+      .where("title", "==", title);
+
+    let previousVotes = await previousVoteQuery
+      .get()
+      .then((snapshots) => snapshots.docs.map((doc) => doc.data().votes));
+
+    details = {
+      progression: calculateRankProgression(rank, previousRanks[0]),
+      previousRank: previousRanks[0],
+      voteDifference: calculateVoteDifference(votes, previousVotes[0]),
+    };
+
+    return details;
+  };
 </script>
 
-<div>{votes}</div>
+{#await fetchDetails()}
+  <div class="container">Loading...</div>
+{:then details}
+  <div class="container">
+    <div class="sub-container">
+      <div class="figure">{details.progression}</div>
+      <div class="label">FROM RANK {details.previousRank}</div>
+    </div>
+    <div class="sub-container">
+      <div class="figure">{votes}</div>
+      <div class="label">{details.voteDifference}</div>
+    </div>
+  </div>
+{/await}
 
 <style>
-  div {
-    width: 6rem;
+  .container {
+    display: flex;
+    flex-direction: column;
+    width: 10rem;
+    height: 100%;
     text-align: center;
+  }
+
+  .sub-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    height: 50%;
+  }
+
+  .figure {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  .label {
+    font-size: 0.8rem;
   }
 </style>
